7dd8607ee7eee40b1728e9e9c3c48326061a2113
fanny vidal <fannyvidal98@gmail.com>
2022-10-15 11:41:44 -0500

    account controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'accounts/index'
+  get 'accounts/show'
+  get 'accounts/new'
+  get 'accounts/create'
+  get 'accounts/edit'
+  get 'accounts/update'
+  get 'accounts/destroy'
   devise_for :users
   root to: 'pages#home'
 end
