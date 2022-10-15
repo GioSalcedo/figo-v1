e9f63b501a3b1e396121aeeb28da04236aa8c9c2
JUAN <jsmith.jdhp@gmail.com>
2022-10-15 11:33:14 -0500

    create controller transferencias

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'bankings/index'
+  get 'bankings/show'
+  get 'bankings/new'
+  get 'bankings/create'
+  get 'bankings/edit'
+  get 'bankings/update'
+  get 'bankings/destroy'
+  get 'bankings/controller_transfer'
+  get 'bankings/index'
+  get 'bankings/repository_transfers'
+  get 'bankings/destroy_transfer'
+  get 'bankings/show_tranfers'
+  get 'bankings/create_transfer'
   devise_for :users
   root to: 'pages#home'
 end
