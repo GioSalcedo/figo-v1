4672f2d15049ca26210a2dacb8c61a77a15ec09f
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-10-15 08:32:32 -0500

    model business and controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
 end
---
+++ db/migrate/20221015133053_create_businesses.rb
@@ -0,0 +1,12 @@
+class CreateBusinesses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :businesses do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+      t.string :logo
+
+      t.timestamps
+    end
+  end
+end
