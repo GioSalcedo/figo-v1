62897f915e3bbb7460522f9709c8574e38e4e91b
fanny vidal <fannyvidal98@gmail.com>
2022-10-15 16:09:12 -0500

    correciones controllers

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,13 +1,12 @@
 # controlador que muestra todos los negocios que puede tener un usuario.
 class BusinessController < ApplicationController
+  before_action :set_business, only: %i[show edit update destroy]
 
   def index
-    @business = Business.all
+    @businesses = Business.all
   end
 
-  def show
-    @business = Business.find(params[:id])
-  end
+  def show; end
 
   def new
     @business = Business.new
@@ -18,29 +17,33 @@ class BusinessController < ApplicationController
     if @business.save
       redirect_to @business, :notice => "Has creado tu empresa de forma exitosa."
     else
-      render :action => 'new'
+      render :new
     end
   end
 
-  def edit
-    @business = Business.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @business = Business.find(params[:id])
-    if @business.update_attributes(params[:business])
+    if @business.update_attributes(business_params)
       redirect_to @business, :notice  => "Has editado tu empresa de forma exitosa."
     else
-      render :action => 'edit'
+      render :edit
     end
   end
 
   def destroy
-    @business = Business.find(params[:id])
     @business.destroy
     redirect_to business_index_url, :notice => "Has eliminado esta empresa de forma exitosa."
   end
+end
 
-  
+private
 
+  def set_business
+    @business = Business.find(params[:id])
+  end
+
+  def business_params
+    params.require(:business).permit(:user_id, :name, :description, :logo)
+  end
 end
