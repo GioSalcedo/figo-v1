7d36d0cd3ae0d3059436ae2bbdfc9f5d43b6c8d5
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-10-15 12:14:26 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users do
+    resources :businesses do
+      resources :bankings
+      resources :earnings
+      resources :egresses
+      resources :debts
+      resources :budgets
+      resources :savings
+      resources :accounts
+    end
+  end
 end
