fb447062c08f8a3a532f441cce316b056c5cb580
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-10-20 22:23:04 -0500

    big changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users do
-    resources :businesses do
-      resources :bankings
-      resources :earnings
-      resources :egresses
-      resources :debts
-      resources :budgets
-      resources :savings
-      resources :accounts
-    end
+  resources :businesses
+  resources :accounts do
+    resources :bankings
+  end
+
+  resources :bankings do
+    resources :earnings
+    resources :egresses
   end
 end
