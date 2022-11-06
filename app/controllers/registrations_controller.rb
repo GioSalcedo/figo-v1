a9bf5528eb215a9c45464d741a0b1f80d52d736f
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-11-05 22:51:55 -0500

    cambios permit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(_resource)
+  def after_sign_up_path_for(resource)
     '/businesses' # Or :prefix_to_your_route
   end
 
-  def after_inactive_sign_up_path_for(_resource)
+  def after_inactive_sign_up_path_for(resource)
     '/businesses' # Or :prefix_to_your_route
   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
   end
 end
